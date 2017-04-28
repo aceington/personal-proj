099f0c57e8fbcd5fd8dc938e2ce21b2f935b07af
Andrew Chen <ac12491@gmail.com>
2017-04-28 17:48:55 -0600

    finished sle section

--- js/jQuery.js
+++ js/jQuery.js
@@ -4,5 +4,21 @@ $(".dropdown-button").dropdown();
 
 $('.carousel').carousel();
 
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
 
 });
