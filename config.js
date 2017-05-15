5b815891d3fda4505ab8e736e6b1adaf3191d158
Andrew Chen <ac12491@gmail.com>
2017-05-15 10:32:32 -0600

    just about done

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionString: "postgres://pnhffynx:tLu1sNE80QLu4O1VmFxtFhpGyaIzuFT8@stampy.db.elephantsql.com:5432/pnhffynx"
+}
--- index.js
+++ index.js
@@ -7,14 +7,13 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
 const conn = massive.connectSync({
-  connectionString: "postgres://postgres@localhost:5433/postgres"
+  connectionString: config.connectionString
 });
 
 app.set('db', conn);
 const db = app.get('db');
 
 app.get('/vehicles/all', function(req, res) {
-  console.log('at srvr');
   db.getAllVehicles(function(err, vehicles) {
     if (!err) {
       console.log(err);
@@ -27,3 +26,4 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log('listening on port: ', PORT);
 })
+` `
